87b62d1943bb6d14c2c1cab502c6681baf731c56
Stephen White <saltybanana72@gmail.com>
2019-09-05 14:59:00 -0700

    Created basic top level files that will be run and manage the rest of the program

---
+++ MainMLTrainer.py
@@ -0,0 +1,2 @@
+#this file will be the main way of training the neural network.
+#THIS CANNOT RUN WHILE THE SEVER IS RUNNING!!!
---
+++ MainServerStarter.py
@@ -0,0 +1,2 @@
+#This file is the main starter for the server side.
+#This will launch the listening sockets, recieve incoming connections, and start new threads for each Pi
