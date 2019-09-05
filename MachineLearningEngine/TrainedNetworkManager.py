eee3f3a790f0f354121ec8a09366fa089c61df55
Stephen White <saltybanana72@gmail.com>
2019-09-05 16:13:11 -0700

    Started the class that manages the writing and reading of trained neural networks

---
+++ MachineLearningEngine/TrainedNetworkManager.py
@@ -0,0 +1,16 @@
+#This class will be the object that manages reading and writing of the tensorflow models.
+#This will also be creating a .csv file to manage the performance of each model.
+
+class TrainedNetworkManager:
+	
+	#this will write the model to the file and also include it in a .csv file
+	def WriteModel():
+
+		pass
+
+	#this function will read in and return the model and all the stats about that model listed in the .csv file.
+	#it should also present a warning if the user is trying to use a model that does not have the highest recorded accuracy.
+	def ReadModel():
+		pass
+
+
